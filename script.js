7b347d8b40fe47cb6c8bc67fd80ea2c4298d5fc4
aliceabbruzzino <aliceabbruzzino7@gmail.com>
2021-12-28 17:29:58 +0100

    Upload
    Rotation sticker intro js

--- script.js
+++ script.js
@@ -1,4 +1,8 @@
-$('.carousel').carousel({
-  interval: 2000,
-  pause: hover
-})
+window.onscroll = function() {
+  scrollRotate();
+};
+
+function scrollRotate() {
+  let image = document.getElementById("reload");
+  image.style.transform = "rotate(" + window.pageYOffset / 1 + "deg)";
+}
